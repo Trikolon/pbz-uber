520c67a35e4457745434da9d8957147328493722
Paul Zuehlcke <1077860+Trikolon@users.noreply.github.com>
2017-03-17 00:29:25 +0100

    Refactored invert and flicker cmd into effect cmd

--- paul.zuehlcke.de/js/main.js
+++ paul.zuehlcke.de/js/main.js
@@ -37,7 +37,7 @@ function JSUtil() {
     }
     else {
         if (Cookies.get("flicker") === "false") { //Is flicker disabled?
-            lwConsole.executeCmd(["flicker", "false"]);
+            lwConsole.executeCmd(["effect", "flicker", "false"]);
         }
     }
     if (typeof invertCookie === "undefined") {
@@ -45,7 +45,7 @@ function JSUtil() {
     }
     else {
         if (Cookies.get("invert") === "true") { //Is invert enabled?
-            lwConsole.executeCmd(["invert", "true"]);
+            lwConsole.executeCmd(["effect", "invert", "true"]);
         }
     }
 
