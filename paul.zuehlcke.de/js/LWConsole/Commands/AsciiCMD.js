c22c053dcd85bc142e1f774306c89602df64fd1e
Trikolon <1077860+Trikolon@users.noreply.github.com>
2017-03-24 23:23:49 +0100

    AsciiCMD: optimisations, code formatting

--- paul.zuehlcke.de/js/LWConsole/Commands/AsciiCMD.js
+++ paul.zuehlcke.de/js/LWConsole/Commands/AsciiCMD.js
@@ -15,7 +15,6 @@
  */
 
 import LWCommand from "../LWCommand";
-import UsageError from "../UsageError";
 
 export default class AsciiCMD extends LWCommand {
     constructor() {
@@ -27,11 +26,11 @@ export default class AsciiCMD extends LWCommand {
         if (args.length > 0) {
             let output = "";
             let iter = 0;
-            while(iter<args.length) {
-                let expression = parseInt(args[iter])+"="+AsciiCMD.getChar(parseInt(args[iter])) + ",";
-                if(expression.length <8) output += expression + "\t\t";
+            while (iter < args.length) {
+                let expression = parseInt(args[iter], 10) + "=" + AsciiCMD.getChar(parseInt(args[iter], 10)) + ",";
+                if (expression.length < 8) output += expression + "\t\t";
                 else output += expression + "\t";
-                if(iter%8 == 7)output += "\n";
+                if (iter % 8 === 7) output += "\n";
                 iter++;
             }
             return output;
@@ -39,11 +38,11 @@ export default class AsciiCMD extends LWCommand {
         else {
             let output = "";
             let iter = 0;
-            while(iter<256) {
-                let expression = iter+"="+AsciiCMD.getChar(iter) + ",";
-                if(expression.length <8) output += expression + "\t\t";
+            while (iter < 256) {
+                let expression = iter + "=" + AsciiCMD.getChar(iter) + ",";
+                if (expression.length < 8) output += expression + "\t\t";
                 else output += expression + "\t";
-                if(iter%8 == 7)output += "\n";
+                if (iter % 8 === 7) output += "\n";
                 iter++;
             }
             return output;
@@ -51,43 +50,49 @@ export default class AsciiCMD extends LWCommand {
         }
     }
 
+
     static getChar(code) {
-        switch(code) {
-            case 0: return "[NUL]";
-            case 1: return  "[SOH]";
-            case 2: return  "[STX]";
-            case 3: return  "[ETX]";
-            case 4: return  "[EOT]";
-            case 5: return  "[ENQ]";
-            case 6: return  "[ACK]";
-            case 7: return  "[BEL]";
-            case 8: return  "[BS]";
-            case 9: return  "[HT]";
-            case 10: return  "[LF]";
-            case 11: return  "[VT]";
-            case 12: return  "[FF]";
-            case 13: return  "[CR]";
-            case 14: return  "[SO]";
-            case 15: return  "[SI]";
-            case 16: return  "[DLE]";
-            case 17: return  "[DC1]";
-            case 18: return  "[DC2]";
-            case 19: return  "[DC3]";
-            case 20: return  "[DC4]";
-            case 21: return  "[NAK]";
-            case 22: return  "[SYN]";
-            case 23: return  "[ETB]";
-            case 24: return  "[CAN]";
-            case 25: return  "[EM]";
-            case 26: return  "[SUB]";
-            case 27: return  "[ESC]";
-            case 28: return  "[FS]";
-            case 29: return  "[GS]";
-            case 30: return  "[RS]";
-            case 31: return  "[US]";
-            case 32: return  "[SP]";
-            case 255: return  "[DEL]";
-            default: return String.fromCharCode(code);
+        let codes = {
+            0: "[NUL]",
+            1: "[SOH]",
+            2: "[STX]",
+            3: "[ETX]",
+            4: "[EOT]",
+            5: "[ENQ]",
+            6: "[ACK]",
+            7: "[BEL]",
+            8: "[BS]",
+            9: "[HT]",
+            10: "[LF]",
+            11: "[VT]",
+            12: "[FF]",
+            13: "[CR]",
+            14: "[SO]",
+            15: "[SI]",
+            16: "[DLE]",
+            17: "[DC1]",
+            18: "[DC2]",
+            19: "[DC3]",
+            20: "[DC4]",
+            21: "[NAK]",
+            22: "[SYN]",
+            23: "[ETB]",
+            24: "[CAN]",
+            25: "[EM]",
+            26: "[SUB]",
+            27: "[ESC]",
+            28: "[FS]",
+            29: "[GS]",
+            30: "[RS]",
+            31: "[US]",
+            32: "[SP]",
+            255: "[DEL]"
+        };
+        if (codes.hasOwnProperty(code)) {
+            return code[code];
+        }
+        else {
+            return String.fromCharCode(code);
         }
     }
 }
