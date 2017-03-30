4286f7ac8f137842105a02fd1674cde1a44099a4
Trikolon <pbz@posteo.de>
2017-03-30 21:17:06 +0200

    wiki cmd: Handle article not found

--- paul.zuehlcke.de/js/LWConsole/Commands/WikiCMD.js
+++ paul.zuehlcke.de/js/LWConsole/Commands/WikiCMD.js
@@ -25,22 +25,30 @@ export default class WikiCMD extends LWCommand {
 
     run(args) {
         let keyword = args.join(" ");
-        let queryUrl = "https://en.wikipedia.org/w/api.php?format=json&action=query&redirects=1&prop=extracts&exintro=&explaintext=&origin=*&titles="+keyword;
+        let queryUrl = "https://en.wikipedia.org/w/api.php?format=json&action=query&redirects=1&prop=extracts&exintro=&explaintext=&origin=*&titles=" + keyword;
         if (args.length < 1) {
             throw new UsageError();
         } else {
             //If the pattern is met:
             let request = new XMLHttpRequest();
             request.onload = () => {
-                if (request.status !== 200) {
-                    this.print("\nError: wikipedia.org returned code " + request.status);
+                try {
+                    if (request.status !== 200) {
+                        throw new Error("wikipedia.org returned code " + request.status);
+                    }
+                    let pages = JSON.parse(request.responseText).query.pages;
+                    if (pages.hasOwnProperty("-1")) {
+                        throw new Error("No article found");
+                    }
+                    let article = pages[Object.keys(pages)[0]];
+                    let text = article.extract;
+                    if (text.length > 1000) text = text.substring(0, 1000) + " [...]";
+                    this.print("\n *** " + article.title + " ***\n\n" + text +
+                        "\n\nFull article under: https://en.wikipedia.org/wiki/" + (article.title.split(" ").join("_")));
                 }
-                let json = JSON.parse(request.responseText);
-                let pages = json.query.pages;
-                let article = pages[Object.keys(pages)[0]];
-                let text = article.extract;
-                if(text.length > 1000) text = text.substring(0,1000) + "...";
-                this.print("\n *** " + article.title + " ***\n\n" + text + "\n\nFull article under: https://en.wikipedia.org/wiki/" + (article.title.split(" ").join("_")) );
+                catch (e) {
+                    this.print(e.name + ": " + e.message);
+                }
             };
             request.onerror = function (e) {
                 console.error(e);
@@ -48,7 +56,7 @@ export default class WikiCMD extends LWCommand {
             };
             request.open("GET", queryUrl, true);
             request.send();
-            return "Getting data ...";
+            return "Loading ...";
         }
     }
 }
