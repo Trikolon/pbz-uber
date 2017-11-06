a27342523fa99d101f7a25dc60ad14d0994af1ef
Trikolon <1077860+Trikolon@users.noreply.github.com>
2017-11-06 21:50:22 +0100

    Fixed build error by using different source map type

--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,86 @@
-const path = require("path");
+const path = require('path');
 const webpack = require('webpack');
-const BabiliPlugin = require("babili-webpack-plugin");
-const OfflinePlugin = require("offline-plugin");
+const BabiliPlugin = require('babili-webpack-plugin');
+const OfflinePlugin = require('offline-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    context: path.resolve("src"),
-    entry: "./index.js",
-    output: {
-        path: path.resolve('public'),
-        publicPath: "/",
-        filename: 'bundle.js'
+  context: path.resolve('src'),
+  entry: './index.js',
+  output: {
+    path: path.resolve('public'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
+    ],
+  },
+  devtool: '#eval-source-map',
+  devServer: {
+    contentBase: path.join('.', 'public'),
+    historyApiFallback: true,
+    noInfo: true,
+  },
+  performance: {
+    hints: false,
+  },
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.esm.js',
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url-loader?limit=10000&mimetype=application/font-woff"
-            },
-            {test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"}
-        ]
-    },
-    devtool: '#eval-source-map',
-    devServer: {
-        contentBase: path.join(".", "public"),
-        historyApiFallback: true,
-        noInfo: true
-    },
-    performance: {
-        hints: false
-    },
-    resolve: {
-        alias: {
-            vue$: "vue/dist/vue.esm.js"
-        }
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html'
-        }),
-        new CopyWebpackPlugin([
-            { from: 'pwa' },
-            { from: 'css'}
-        ])
-    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new CopyWebpackPlugin([
+      { from: 'pwa' },
+      { from: 'css' },
+    ]),
+  ],
 };
 
 if (process.env.NODE_ENV === 'production') { // only add in production
-    module.exports.devtool = '#source-map';
-    // http://vue-loader.vuejs.org/en/workflow/production.html
-    module.exports.plugins = (module.exports.plugins || []).concat([
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        }),
-        new BabiliPlugin(),
-        new webpack.LoaderOptionsPlugin({
-            minimize: true
-        }),
-        new OfflinePlugin({
-            caches: {
-                main: [
-                    "bundle.js",
-                    "index.html"
-                ]
-            },
-            autoUpdate: true
-        })
-    ])
-}
-else { // Only add in development
-    module.exports.plugins = (module.exports.plugins || []).concat([
-        new webpack.NamedModulesPlugin()
-    ])
+  module.exports.devtool = undefined;
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"',
+      },
+    }),
+    new BabiliPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+    }),
+    new OfflinePlugin({
+      caches: {
+        main: [
+          'bundle.js',
+          'index.html',
+        ],
+      },
+      autoUpdate: true,
+    }),
+  ]);
+} else { // Only add in development
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.NamedModulesPlugin(),
+  ]);
 }
